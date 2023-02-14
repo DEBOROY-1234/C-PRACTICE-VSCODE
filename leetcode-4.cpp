3be039b51e11351cfedf2f7d26e216a29f2f0ce6
DEBOROY-1234 <deboroy2002@gmail.com>
2023-02-14 20:01:58 +0530

    Majority Element

---
+++ leetcode-4.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        
+        int n = nums.size();
+        unordered_map<int,int> mp;
+        
+        for(auto i : nums){
+            mp[i]++;
+        }
+        
+        vector<int> ans;
+        
+        for(auto it : mp){
+            if(it.second>n/3){
+                ans.push_back(it.first);
+            }
+        }
+        
+        return ans;
+        
+    }
+};
